97507be76fff2593967661531ae214a9788b06f6
Kirsten Ali <ikirstenhd@gmail.com>
2023-06-22 19:05:45 +0100

    ImageProcessor class added.

---
+++ src/main/java/org/aviansoundscape/training/preprocessing/ImageProcessor.java
@@ -0,0 +1,8 @@
+package org.aviansoundscape.training.preprocessing;
+
+public class ImageProcessor implements FileProcessor{
+    @Override
+    public double[] processFile(String filePath) {
+        return new double[0]; //TODO
+    }
+}
